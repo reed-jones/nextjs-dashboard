90b3273c6cd34239ea17da71bbd1a838f75369b5
reed-jones <reedjones@reedjones.com>
2024-03-20 12:53:09 -0700

    wip

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,26 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+import { lusitana } from '@/app/ui/fonts';
+import Pagination from '@/app/ui/invoices/pagination';
+import Table from '@/app/ui/customers/table';
+import Search from '@/app/ui/search';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string; page?: string };
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <Table customers={customers} />
+    </div>
+  );
 }
---
+++ app/lib/actions.ts
@@ -0,0 +1,119 @@
+'use server';
+
+import { signIn } from '@/auth';
+import { sql } from '@vercel/postgres';
+import { AuthError } from 'next-auth';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { z } from 'zod';
+
+export async function authenicate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid Credentials';
+        default:
+          return 'Something weny wrong';
+      }
+    }
+    throw error;
+  }
+}
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer',
+    required_error: 'Please select a customer',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0' })
+    .transform((val) => val * 100),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status',
+    required_error: 'Please select an invoice status',
+  }),
+  date: z.string(),
+});
+
+// This is temporary until @types/react-dom is updated
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse(
+    Object.fromEntries(formData.entries()),
+  );
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+  const { customerId, amount, status } = validatedFields.data;
+
+  const date = new Date().toISOString().split('T')[0];
+
+  try {
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amount}, ${status}, ${date})
+  `;
+  } catch {
+    return {
+      message: `Database Error: Failed to Create Invoice`,
+    };
+  }
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse(
+    Object.fromEntries(formData.entries()),
+  );
+
+  try {
+    await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amount}, status = ${status}
+    WHERE id = ${id}
+  `;
+  } catch {
+    return {
+      message: `Database Error: Failed to Update Invoice`,
+    };
+  }
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  throw new Error('Failed to Delete Invoice');
+
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch {
+    return {
+      message: `Database Error: Failed to Delete Invoice`,
+    };
+  }
+  revalidatePath('/dashboard/invoices');
+}
